cbe834c83ae60cc95907f9e295a9030f2e28cff2
ethanysma <ethanysma@tencent.com>
2020-09-22 15:56:23 +0800

    Initial Commit

---
+++ OCSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCSample
+//
+//  Created by ethanysma on 2020/9/22.
+//  Copyright © 2020 yishengma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
